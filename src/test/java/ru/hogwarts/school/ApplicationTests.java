2d023ede036eaa897602f5b519271af43b259000
Константин <gkv110592@yandex.ru>
2023-07-11 20:38:10 +0300

    Initial commit

---
+++ src/test/java/ru/hogwarts/school/ApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.hogwarts.school;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@OpenAPIDefinition
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
